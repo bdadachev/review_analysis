d403c53989619a11551ef15fc9433649269e8284
Boris Dadachev <boris.dadachev@gmail.com>
2015-02-15 12:35:54 -0500

    Adding config parameters

--- src/config/data_config.py
+++ src/config/data_config.py
@@ -1,9 +1,11 @@
 import os
 
-# os.getcwd() will be the src/ folder
+# os.getcwd() is the src/ folder (assume all scripts are run from there)
 DATA_ROOT = os.path.abspath(os.path.join(os.getcwd(), "../data/"))
 
 # folder containing the raw files downloaded from Yelp
 YELP_RAW_DATA_FOLDER = os.path.join(DATA_ROOT, "raw/yelp_challenge_dataset/")
-# folder containing the yelp files after munging 
+# folder containing the Yelp files after munging 
 YELP_PICKLE_DATA_FOLDER = os.path.join(DATA_ROOT, "pickle/yelp_challenge_dataset/") 
+# folder storing the various models built
+MODELS_FOLDER = os.path.join(DATA_ROOT, "models/")
--- src/config/django_config_template.py
+++ src/config/django_config_template.py
@@ -1,7 +1,33 @@
+"""
+Check https://docs.djangoproject.com/en/1.7/ref/settings/ for details.
+"""
+
+# secret key for django security features; customize and keep a secret
 SECRET_KEY = 'MY_SECRET_KEY'
+# customize if you want Google Analytics
 GOOGLE_ANALYTICS_CODE = 'MY_GOOGLE_ANALYTICS_ID'
 
+# keep these to False in production
 DEBUG = False
 TEMPLATE_DEBUG = False
+# host(s) served by the app
 ALLOWED_HOSTS = ["MY_SERVER_URL"]
-ADMINS = ["MY_ADMIN@MY_DOMAIN.COM"]
+
+# list of people notified when the app raises an exception
+ADMINS = [("MY_ADMIN_NAME", "MY_ADMIN@SOME_DOMAIN.COM")]
+# from email, when the app raises an exception
+SERVER_EMAIL = "django@MY_SERVER_URL"
+
+# How to send emails?
+# - console.EmailBackend: use for debug, writes emails to the console
+# - smtp.EmailBackend: use for production, sends emails to ADMINS when exceptions occur 
+# (if you use Heroku, you can use the addon/email-server MailGun)
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# settings for email server and authentication 
+# (for smtp.EmailBackend only, you can comment all this is if use console.EmailBackend)
+EMAIL_USE_TLS = True
+EMAIL_HOST = "MY_EMAIL_HOST"
+EMAIL_HOST_USER = "MY_EMAIL_USER"
+EMAIL_HOST_PASSWORD = "MY_EMAIL_PASSWORD"
+EMAIL_PORT = "MY_EMAIL_PORT"
